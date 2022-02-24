77560ba993c59fb9342dc64ce20bb9c248b97ee2
Ty Hill <hill.tyj22@gmail.com>
2022-02-24 15:01:19 -0700

    added rollbar

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const path = require('path')
 
+// roll bar stuffs
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '8c49d6c1133741539d72c31ba90cf89b',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// generic message for rollbar
+rollbar.log('Hello world!')
+// roll bar stuffs
+
 const app = express()
 
 app.get('/', (req, res) => {
