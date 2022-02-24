0223c582c10a6ab69aaebb8e88835f1e445fd9ed
Ty Hill <hill.tyj22@gmail.com>
2022-02-24 16:15:11 -0700

    add buttons and button func

---
+++ public/index.js
@@ -0,0 +1,36 @@
+console.log('front end java right here bby')
+
+const sendInfo = () => {
+    console.log('sent info')
+    axios.get('/api/info').then(res => {
+        alert(res.data)
+    }).catch(err => console.log(err))
+}
+
+const sendCritical = () => {
+    console.log('sent critical')
+    axios.get('/api/critcal').then(res => {
+        alert(res.data)
+    }).catch(err => console.log(err))
+}
+
+const sendWarning = () => {
+    console.log('sent warning')
+    axios.get('/api/warning').then(res => {
+        alert(res.data)
+    }).catch(err => console.log(err))
+}
+
+const sendError = () => {
+    console.log('sent error')
+    axios.get('/api/error').then(res => {
+        alert(res.data)
+    }).catch(err => console.log(err))
+}
+
+// buttons
+
+document.getElementById('information').addEventListener('click', sendInfo)
+document.getElementById('critical').addEventListener('click', sendCritical)
+document.getElementById('warning').addEventListener('click', sendWarning)
+document.getElementById('error').addEventListener('click', sendError)
--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const path = require('path')
 
 // roll bar stuffs
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
   accessToken: '8c49d6c1133741539d72c31ba90cf89b',
   captureUncaught: true,
   captureUnhandledRejections: true,
@@ -15,9 +15,44 @@ rollbar.log('Hello world!')
 
 const app = express()
 
+app.use(express.json)
+app.use(rollbar.errorHandler())
+
+// get endpoints here and shi
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
+
+// roll bar stuffs pt 2
+app.get('/api/info', (req, res) => {
+    res.status(200).send('rollbar got sent some yummy info')
+    rollbar.info('here is that rollbar info my guy')
+})
+
+app.get('/api/critical', (req, res) => {
+    res.status(200).send('rollbar got sent a not so yummy crital error')
+    rollbar.critical('rollbar critial senteth')
+})
+
+app.get('/api/warning', (req, res) => {
+    res.status(200).send('rollbar got sent a not so yummy warning')
+    rollbar.warning('here is ya warning g')
+})
+
+app.get('/api/error', (req, res) => {
+    res.status(200).send('rollbar got sent an error cuz of u')
+    rollbar.error('rollbar error sented')
+})
+
+// connecting some stuffs
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.js'))
+})
+
+app.get('/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/styles.css'))
+})
+
 
 const port = process.env.PORT || 4204
 
